3e35129f5a695124a5d70d675acdb19dd5b95e87
kp047i <takayuki.miura28@gmail.com>
2020-07-21 14:11:23 +0900

    style: display sample articles

---
+++ pages/articles/_slug.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <h1>{{ article.title }}</h1>
+    <nuxt-content :document="article" />
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const article = await $content(`articles/${params.slug}`).fetch()
+    return {
+      article
+    }
+  }
+}
+</script>
