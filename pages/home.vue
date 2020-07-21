4be0f31c654dc45553ec534f2668dca9807fe5d6
kp047i <takayuki.miura28@gmail.com>
2020-07-21 14:10:53 +0900

    styel: display home content

---
+++ pages/home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <nuxt-content :document="page" />
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content }) {
+    const page = await $content('home').fetch()
+    return {
+      page
+    }
+  }
+}
+</script>
